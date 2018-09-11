7956137fcf68a226d542a5bbbdbd450607c6c914
mikelprincesa4 <mikelprincesa@gmail.com>
2018-09-11 06:58:28 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Main {
+	
+	public static File 				f1 = new File("2017.txt");
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+	}
+	
+	public static void readStats() {
+		
+	}
+
+}
---
+++ src/Player.java
@@ -0,0 +1,8 @@
+
+public class Player {
+	String name;		//player name
+	String team;		//team
+	String position;	//position
+	int totPnts;		//total fantasy points during season
+	int posRank;		//position rank
+}
