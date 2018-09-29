d3c4eeefba17844fe5a3494f0fbd222269101f85
Mikel Princesa <mikelprincesa@gmail.com>
2018-09-29 00:00:01 -0400

    Last player statistics fixed

--- src/Main.java
+++ src/Main.java
@@ -5,63 +5,55 @@ import java.util.*;
 public class Main {
 	
 	public static File				f1 		= new File("2017.txt");
-	static Player[] 			players;
+	static Player[] 				players;
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		String ignore1, ignore2, unformatted;
 		Scanner stdin 		= new Scanner(f1);
+		Scanner usrin		= new Scanner(System.in);
 		int	playerCount 	= countPlayers(f1);
 		int curPlayerNum	= 0;
-		players = new Player[playerCount];
-		
-		
-		ignore1 	= stdin.nextLine();		// Ignores first line of text (table labels in CSV)
-		ignore2 	= stdin.nextLine();		// Ignores second line of text (table labels in CSV)
-		ignore1 	= null;					// Free up memory
-		ignore2 	= null;					// Free up memory
+		players 			= new Player[playerCount];
+				
+		ignore1 	= stdin.nextLine();										// Ignores first line of text (table labels in CSV)
+		ignore2 	= stdin.nextLine();										// Ignores second line of text (table labels in CSV)
+		ignore1 	= null;													// Free up memory
+		ignore2 	= null;													// Free up memory
 		unformatted = stdin.nextLine();
 		
-		while (stdin.hasNextLine()) {		// Populate Player[]
+		while (curPlayerNum < playerCount) {								// Populate Player[]
 			makePlayer(unformatted, playerCount, curPlayerNum);
-			unformatted = stdin.nextLine();
+			try {
+				unformatted = stdin.nextLine();
+			} catch (NoSuchElementException e) {
+				break;
+			}
 			curPlayerNum++;	
 		}
 		stdin.close();
 		
 	}
-
 	
 	public static void makePlayer(String unformatted, int playerCount, int curPlayerNum) {
 		String	formatted;
-		String	firstName, lastName, team, position, skip, posTemp;
+		String	firstName, lastName, team, position, skip;
 		int		playerNum, posRank;
 		Scanner	s = new Scanner(unformatted);
 		
-		formatted = unformatted.replace('*','\\');			// Replace * with \ for formatting purposes
+		formatted = unformatted.replace('*','\\');							// Replace * with \ for formatting purposes
 		s.useDelimiter("[\\s\\*\\\\?\\,\\+\\_\\@]+");
 		playerNum 		= s.nextInt();
 		firstName 		= s.next();
 		lastName  		= s.next();
 		skip 			= s.next();
 		team			= s.next();
-		posTemp			= s.next();
-		if (posTemp == "") {								// Correct when player has no position listed
-			position	= "n/a";
-		} else {
-			position	= posTemp;
-		}
+		position		= s.next();
 		s.useDelimiter(",");
-		for (int i=0; i < 24; i++) {
-			skip 	= s.next();
+		for (int i=0; i < 24; i++) {						// Skip unused statistics
+			skip 		= s.next();	
 		}
-		posRank		= s.nextInt();
+		posRank			= s.nextInt();
 		players[curPlayerNum] = new Player (firstName,lastName,team, position, posRank);
-		
-		System.out.println(players[curPlayerNum].getFirstName());
-		System.out.println(players[curPlayerNum].getLastName());
-		System.out.println(players[curPlayerNum].getTeam());
-		System.out.println(players[curPlayerNum].getPosition());
-		System.out.println(players[curPlayerNum].getPosRank());
 	}
 	
 	public static int countPlayers(File fileName) throws FileNotFoundException{
